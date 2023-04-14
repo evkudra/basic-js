9672e6387bbb29a1f3a73a195fd91fb5d36a9d1b
Evgeniy Kudra <evkudra@gmail.com>
2023-04-14 13:09:05 +0300

    done matrix elements sum

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -9,16 +9,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * @example
  * matrix = [
- *  [0, 1, 1, 2],
- *  [0, 5, 0, 0],
- *  [2, 0, 3, 3]
+ *  [0, 1, 1, 2],   0 0
+ *  [0, 5, 0, 0],   1 0
+ *  [2, 0, 3, 3]    2 0
  * ]
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
+function getMatrixElementsSum( matrix ) {
+  console.dir(matrix)
+  let countColumn = matrix[0].length;
+  let sum = 0;
+  for (let i = 0; i < countColumn; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if (matrix[j][i] === 0) {
+        break
+      } else {
+        sum += matrix[j][i];
+      }
+      
+    }
+  } 
+  console.dir(sum)
+  return sum
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -16,6 +16,39 @@ const { NotImplementedError } = require('../extensions/index.js');
 function transform(/* arr */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  // let transformArr = [];
+  // if (Array.isArray(arr) === false) {
+  //   throw new Error('arr parameter must be an instance of the Array!') 
+  // }
+ 
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (typeof arr[i] !== 'number' || typeof arr[i] !== 'string') {
+  //       return 'arr parameter must be an instance of the Array!'
+  //     }
+  //     if (arr[i] === '--double-next') {
+  //       transformArr.push(arr[i+1]);
+  //       continue
+  //     }
+  //     if (arr[i] === '--double-prev') {
+  //       transformArr.push(arr[i-1]);
+  //       continue
+  //     }
+  //     if (arr[i] === '--discard-prev') {
+  //       transformArr.pop();
+  //       continue
+  //     }
+  //     if (arr[i] === '--discard-next') {
+  //       transformArr.pop();
+  //       i++;
+  //       continue
+  //     }
+  //     transformArr.push(arr[i]);
+  //   }
+  
+  
+  // console.dir(arr);
+  // console.dir(transformArr);
+  // return transformArr
 }
 
 module.exports = {
